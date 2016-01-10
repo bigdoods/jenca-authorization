743b634c2c3f2bc48a810dbd2578db6dfe47a9b5
Kai Davenport <kaiyadavenport@gmail.com>
2016-01-10 15:31:28 +0000

    allow the loading of middleware from a config string

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ var Router = require('./router')
 
 var args = require('minimist')(process.argv, {
   alias:{
-    p:'port'
+    p:'port',
+    m:'middleware'
   },
   default:{
-    port:process.env.PORT || 80
+    port:process.env.PORT || 80,
+    middleware:process.env.MIDDLEWARE || 'allowall'
   }
 })
 
+var middleware = []
+if(args.middleware){
+  var parts = args.middleware.split(',')
+  middleware = parts.map(function(name){
+    if(!fs.existsSync(path.join(__dirname, 'middleware', name))){
+      throw new Error('middleware: ' + name + ' does not exist')
+    }
+    return require('./middleware/' + name)(process.env)
+  })
+}
+
 var router = Router({
-  
+  middleware:middleware
 })
 
-var server = http.createServer(router)
+var server = http.createServer(router.handler)
 
 server.listen(args.port, function(err){
   if(err){
