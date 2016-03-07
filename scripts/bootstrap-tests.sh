8485e9129b0b8c9c9147ed1c52651e034ecb057b
Daniel Craig <daniel@danielcraig.me>
2016-03-07 14:33:40 +0000

    print all environment variables

--- scripts/bootstrap-tests.sh
+++ scripts/bootstrap-tests.sh
@@ -5,6 +5,8 @@ export POSTGRES_HOST=${POSTGRES_HOST:="172.17.8.150"}
 export POSTGRES_USER=${POSTGRES_USER:="username"}
 export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:="password"}
 echo $POSTGRES_HOST
+printenv
+
 echo "waiting for postgres"
 sleep 5
 DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/jenca-authorisation" pg-migrate up
