c198d9cc7db0af08386177d416819220b3278a54
Daniel Craig <daniel@danielcraig.me>
2016-03-07 13:50:07 +0000

    take postgres connection info from environment variables

--- scripts/bootstrap-tests.sh
+++ scripts/bootstrap-tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 export POSTGRES_HOST=${POSTGRES_HOST:="172.17.8.150"}
+export POSTGRES_USERNAME=${POSTGRES_USERNAME:="username"}
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:="password"}
 
 echo "waiting for postgres"
 sleep 5
-DATABASE_URL="postgres://username:password@${POSTGRES_HOST}/jenca-authorisation" pg-migrate up
+DATABASE_URL="postgres://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/jenca-authorisation" pg-migrate up
 echo "waiting for tests"
 sleep 5
 node test.js
